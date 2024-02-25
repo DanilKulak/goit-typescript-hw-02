27825f89515d64845069d5df6a1f02f6de945149
DanilKulak <1998kylak@gmail.com>
2024-02-25 15:28:47 +0200

    new-commit-done

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,23 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
+  weight: number;
 }
 
-function compare (top, bottom): AllType {
+function compare<T extends AllType, U extends AllType>(top: Pick<T, keyof AllType>, bottom: Pick<U, keyof AllType>): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
+
+const topObj = { name: 'top', position: 1, color: 'red', weight: 10 };
+const bottomObj = { name: 'bottom', position: 2, color: 'blue', weight: 20 };
+
+const comparedObj = compare(topObj, bottomObj);
+console.log(comparedObj);
+
 
 export {};
